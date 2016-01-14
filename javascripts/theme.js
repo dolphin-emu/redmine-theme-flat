bacd8fb044e78db930e5544a4da039d0b74cadce
Pringo <Pringo@users.noreply.github.com>
2016-01-13 16:59:23 -0800

    Update Capitalization
    Fix 'GitHub' capitalization and make website link capitalization consistent with other links (both 'website' and 'project' should probably be either capitalized or not.)

--- javascripts/theme.js
+++ javascripts/theme.js
@@ -14,9 +14,9 @@
       $(this).closest('fieldset').toggleClass('show-children');
     });
 
-    // add fork us on github link
-    var menu = $("#main-menu>ul").append("<li class='float'><a href='https://dolphin-emu.org/'>Official website</a></li>");
-    var menu = $("#main-menu>ul").append("<li class='float'><a href='https://github.com/dolphin-emu/dolphin'>Github Project</a></li>");
+    // add fork us on GitHub link
+    var menu = $("#main-menu>ul").append("<li class='float'><a href='https://dolphin-emu.org/'>Official Website</a></li>");
+    var menu = $("#main-menu>ul").append("<li class='float'><a href='https://github.com/dolphin-emu/dolphin'>GitHub Project</a></li>");
     var menu = $("#main-menu>ul").append("<li class='float'><a href='https://forums.dolphin-emu.org/'>Forums</a></li>");
     var menu = $("#main-menu>ul").append("<li class='float'><a href='https://wiki.dolphin-emu.org/'>Wiki</a></li>");
   });
